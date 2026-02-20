0eb9143d3333dd5dfba0830904a626c9a4aae0e8
Christian Nagel <chris.imx@online.de>
2026-02-20 22:52:20 +0100

    fix: map CertPathValidatorException to UntrustedCertificate (CIO engine on JVM)

--- esclkt-main/src/jvmMain/kotlin/io/github/chrisimx/esclkt/ESCLHttpCallError.jvm.kt
+++ esclkt-main/src/jvmMain/kotlin/io/github/chrisimx/esclkt/ESCLHttpCallError.jvm.kt
@@ -2,10 +2,11 @@ package io.github.chrisimx.esclkt
 
 import io.ktor.util.network.UnresolvedAddressException
 import java.security.cert.CertPathBuilderException
+import java.security.cert.CertPathValidatorException
 
 actual fun <T> platformErrorMapping(e: Any): ESCLHttpCallResult<T> =
     when (e) {
         is UnresolvedAddressException -> ESCLHttpCallResult.Error.NetworkError(e)
-        is CertPathBuilderException -> ESCLHttpCallResult.Error.UntrustedCertificate()
+        is CertPathBuilderException, is CertPathValidatorException -> ESCLHttpCallResult.Error.UntrustedCertificate()
         else -> ESCLHttpCallResult.Error.UnknownError(e)
     }
