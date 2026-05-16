b05220bf48cadf10339d2dd617e79ae15c6130c3
Christian Nagel <chris.imx@online.de>
2026-05-16 15:18:11 +0200

    fix: update JVM platform error mapping

--- esclkt-main/src/jvmMain/kotlin/io/github/chrisimx/esclkt/ESCLHttpCallError.jvm.kt
+++ esclkt-main/src/jvmMain/kotlin/io/github/chrisimx/esclkt/ESCLHttpCallError.jvm.kt
@@ -3,10 +3,12 @@ package io.github.chrisimx.esclkt
 import io.ktor.util.network.UnresolvedAddressException
 import java.security.cert.CertPathBuilderException
 import java.security.cert.CertPathValidatorException
+import javax.net.ssl.SSLHandshakeException
 
 actual fun <T> platformErrorMapping(e: Any): ESCLHttpCallResult<T> =
     when (e) {
         is UnresolvedAddressException -> ESCLHttpCallResult.Error.NetworkError(e)
-        is CertPathBuilderException, is CertPathValidatorException -> ESCLHttpCallResult.Error.UntrustedCertificate(e.message)
+        is CertPathBuilderException, is CertPathValidatorException, is SSLHandshakeException
+            -> ESCLHttpCallResult.Error.UntrustedCertificate(e.message)
         else -> ESCLHttpCallResult.Error.UnknownError(e)
     }
